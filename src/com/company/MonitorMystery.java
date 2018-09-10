fe58901f34efadb653e98d6180857b551990d581
komesergey <komesergey@gmail.com>
2018-09-10 10:55:01 +0300

    Added example with manual monitorEnter/monitorExit

---
+++ src/com/company/MonitorMystery.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import sun.misc.Unsafe;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+@SuppressWarnings("all")
+public class MonitorMystery {
+    public static Unsafe getUnsafe() {
+        try {
+            for (Field field : Unsafe.class.getDeclaredFields()) {
+                if (Modifier.isStatic(field.getModifiers())) {
+                    if (field.getType() == Unsafe.class) {
+                        field.setAccessible(true);
+                        return (Unsafe) field.get(null);
+                    }
+                }
+            }
+            throw new IllegalStateException("Unsafe field not found");
+        } catch (Exception e) {
+            throw new IllegalStateException("Could not initialize unsafe",e);
+        }
+    }
+
+    public static void main(String... args) throws InterruptedException {
+        Thread t = new Thread() {
+            public void run() {
+                getUnsafe().monitorEnter(MonitorMystery.class);
+                try{
+                    Thread.sleep(10_000);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                getUnsafe().monitorExit(MonitorMystery.class);
+            }
+        };
+        t.start();
+        Thread.sleep(100);
+        System.out.println("Trying to synchronize");
+        System.out.println("Waiting...");
+        synchronized (MonitorMystery.class) {
+            System.out.println("Managed to synchronized");
+        }
+    }
+}
