7e35083468db5e48904e93129668b2b27409fed0
komesergey <komesergey@gmail.com>
2019-02-04 23:13:55 +0300

    Update

--- src/com/company/DSTest.java
+++ src/com/company/DSTest.java
@@ -3,50 +3,16 @@ package com.company;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.Timestamp;
 import java.util.UUID;
 
-/**
- * -- Create table
-     create table PERSONS
-     (
-     id        VARCHAR2(100) not null,
-     firstname VARCHAR2(300),
-     lastname  VARCHAR2(300)
-     )
-     tablespace USERS
-     pctfree 10
-     initrans 1
-     maxtrans 255
-     storage
-     (
-     initial 64K
-     next 1M
-     minextents 1
-     maxextents unlimited
-     );
-     -- Create/Recreate primary, unique and foreign key constraints
-     alter table PERSONS
-     add constraint PK primary key (ID)
-     using index
-     tablespace USERS
-     pctfree 10
-     initrans 2
-     maxtrans 255
-     storage
-     (
-     initial 64K
-     next 1M
-     minextents 1
-     maxextents unlimited
-     );
- */
 public class DSTest {
     public static void main(String[] args){
 
         try {
             ComboPooledDataSource ods = new ComboPooledDataSource();
             ods.setDriverClass("oracle.jdbc.pool.OracleDataSource");
-            ods.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:orcl");
+            ods.setJdbcUrl("jdbc:oracle:thin:@192.168.1.45:1521:orcl");
             ods.setUser("kronos");
             ods.setPassword("KRONOS");
             ods.setMinPoolSize(5);
@@ -54,18 +20,22 @@ public class DSTest {
 
             long start = System.currentTimeMillis();
 
-            for(int i = 0; i < 1_000_000; i++){
+            for(int i = 0; i < 600_000; i++){
                 Connection conn = ods.getConnection();
                 conn.setAutoCommit(false);
-                PreparedStatement ps = conn.prepareStatement("INSERT INTO PERSONS (id, firstname, lastname) VALUES (?,?,?)");
+                PreparedStatement ps = conn.prepareStatement("INSERT INTO ERRORINFO (id, firstname, lastname, servicetypecode, servicecode, updatetime) VALUES (?,?,?,?,?,?)");
                 String key = UUID.randomUUID().toString();
                 ps.setString(1, key);
                 ps.setString(2, "TEST " + key);
                 ps.setString(3, "TESTOV " + key);
+                ps.setString(4, "servicetypecode " + key);
+                ps.setString(5, "servicecode " + key);
+                ps.setTimestamp(6, new Timestamp(System.currentTimeMillis()));
                 ps.executeUpdate();
                 conn.commit();
                 ps.close();
                 conn.close();
+                if(i % 1_000 == 0) System.out.println(i + " " + (System.currentTimeMillis()  - start)) ;
             }
             // Finished in 504936 ms
             System.out.println("Finished in " + (System.currentTimeMillis()  - start) + " ms");
