fe4ce017b60a1038a7e2ed4be9576d591e7551a9
komesergey <komesergey@gmail.com>
2019-04-16 16:20:10 +0300

    Added example with various modifiers for classes

---
+++ src/com/company/TestClassModifiers.java
@@ -0,0 +1,16 @@
+package com.company;
+
+class TestClassModifiers { // public or package-private + final, abstract
+    public class TestInner{ // public, private, protected, package-private + static, final, abstract
+
+    }
+
+    public static void testMethodOne(){
+        class Local {} // package-private + final, abstract
+    }
+
+    public void testMethodTwo(){
+        class Local {} // package-private + final, abstract
+
+    }
+}
