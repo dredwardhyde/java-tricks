70946cb44029b9bef3b6173ed07318454d627bdc
komesergey <komesergey@gmail.com>
2019-07-07 03:34:20 +0300

    strictfp is useless on modern CPUs

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -2,24 +2,31 @@ package com.company;
 
 import java.math.BigDecimal;
 
-public strictfp class Test {
+public class Test {
 
-    private static double d = 7.555d;
+    private static double d = 7.00000000000000000000000001d;
 
     public static strictfp void sout() {
-        BigDecimal bigDecimal = new BigDecimal(d);
+        BigDecimal bigDecimal = new BigDecimal(2.0 - 1.1);
         System.out.println(bigDecimal);
+    }
+
+    public static strictfp void sout2() {
+        System.out.println(2.0 - 1.1);
     }
 
     public static void nope() {
-        BigDecimal bigDecimal = new BigDecimal(d);
-        System.out.println(bigDecimal);
+        System.out.println(2.0 - 1.1);
     }
 
+    /*
+        0.899999999999999911182158029987476766109466552734375
+        0.8999999999999999
+        0.8999999999999999
+     */
     public static void main(String[] args) {
         sout();
+        sout2();
         nope();
-        System.out.println(d);
     }
-
 }
